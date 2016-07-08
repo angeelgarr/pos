70f80816739a449c3e84a069026dc308ab01bcd3
xiehuan <2429753441@qq.com>
2016-07-08 10:39:55 +0800

    完善几个隐藏bug
    1.序号接收不对的情况，改成丢包之后重新接收
    2.PortOpen()放在while(1)函数外面
    3.main函数中采用分支函数，便于代码查看和编写简洁

--- comm_test/main.c
+++ comm_test/main.c
@@ -105,7 +105,7 @@ void pack_up(const char *in_data,ushort *data_len,char *out_data)
 #endif
 	if(in_data==NULL || *data_len==0 || out_data==NULL)
 	{
-		ScrClrLine(0,1);
+		ScrClrLine(0,3);
 		ScrPrint(0,1,1,"param ertempLen+7r\n");
 		return ;
 	}
@@ -132,7 +132,7 @@ void pack_up(const char *in_data,ushort *data_len,char *out_data)
 
 int rcv_packet(char *packet,ushort *pack_len)
 {
-	int comRet;
+	int comRet,iRet;
 	uchar getData[3000];
 	uchar *dataPoint;
 	uint offset,tempLen;
@@ -147,10 +147,10 @@ int rcv_packet(char *packet,ushort *pack_len)
 	{
 		if(!TimerCheck(0))
 		{
-			ScrClrLine(0,1);
+			ScrClrLine(0,3);
 			ScrPrint(0,0,1,"COM timeout\n");
 			getkey();
-			ScrClrLine(0,1);
+			ScrClrLine(0,3);
 			ScrPrint(0,0,1,"SENDING DATA...");
 			return 2;
 		}
@@ -158,7 +158,7 @@ int rcv_packet(char *packet,ushort *pack_len)
 		if(comRet)return 2;
 		if(dataPoint[0]!=0X02)
 		{
-			ScrClrLine(0,1);
+			ScrClrLine(0,3);
 			ScrPrint(0,0,1,"Accept ERR");
 			return 2;
 		}
@@ -166,7 +166,7 @@ int rcv_packet(char *packet,ushort *pack_len)
 		if(offset==tempLen+7) break;
 		if(offset>2048)
 		{
-			ScrClrLine(0,1);
+			ScrClrLine(0,3);
 			ScrPrint(0,0,1,"OVER Arrange");
 			getkey();
 			return 2;
@@ -174,26 +174,158 @@ int rcv_packet(char *packet,ushort *pack_len)
 //		if(glRecCount==0)
 //		ScrPrint(0,2,1,"0X%02X",dataPoint[0]);//屏幕查看发送情况
 	}
-	memcpy(packet,getData,offset);
-	*pack_len=offset;
-    getCRC=get_crc16_short(packet+1,offset-3);  //verify as crc value
+
+    getCRC=get_crc16_short(getData+1,offset-3);  //verify as crc value
 	bufCRC[0]=(getCRC>>8) & 0xFF;
 	bufCRC[1]=getCRC & 0xFF;
-	if(bufCRC[0]!=packet[offset-2] || bufCRC[1]!=packet[offset-1])
+	if(bufCRC[0]!=getData[offset-2] || bufCRC[1]!=getData[offset-1])
 	{
-		ScrClrLine(0,1);
+		ScrClrLine(0,3);
 		ScrPrint(0,0,1,"CRC DIFF,%d",offset);
-		getkey();
 		return 2;
 	}
-	if(glCount!=((packet[1]<<8)+packet[2]))
+	if((glCount-1)!=((getData[1]<<8)+getData[2]))
 	{
-		ScrClrLine(0,1);
-		ScrPrint(0,0,1,"SEQ ERR,%d",glCount);
-		getkey();
-		return 2;
+		ScrClrLine(0,3);
+		ScrPrint(0,0,1,"SEQ ERR,%d,%d",glCount-1,(getData[1]<<8)+getData[2]);
+		iRet=rcv_packet(packet,pack_len);
+		switch(iRet)
+		{
+		case 0:
+			return 0;
+		case 2:
+			return 2;
+		}
 	}
+	memcpy(packet,getData,offset);
+	*pack_len=offset;
 	write(glFd,packet+5,offset-7);
+	return 0;
+}
+uchar GeneData(int fd)
+{
+	int i,fRet;
+	uchar randomBuf[9];
+
+	for(i=0;i<12800;i++)
+		{
+        memset(randomBuf,0,sizeof(randomBuf));
+        PciGetRandom(randomBuf);
+        fRet=write(fd,randomBuf,8);
+        if(fRet!=8)
+        {
+            ScrClrLine(0,3);
+            ScrPrint(0,0,1,"file write err%d",i);
+            return 1;
+        }
+		}
+	return 0;
+}
+
+uchar SendRecvData(int fd)
+{
+	uchar bufSend[3000],bufRec[3000],outputBuf[3000];
+	uchar getAllRecv[10241];uchar getAllSend[10241];
+    uint i,j,count,ret,comRet,lenSend;
+	ushort outputLen,fRet;
+	int fdRet;
+
+	lenSend=102400;
+	fdRet=seek(fd,0,SEEK_SET);
+	if(fdRet<0)
+    {
+        ScrClrLine(0,3);
+        ScrPrint(0,0,1,"open file errr%d",errno);
+        return 1;
+    }
+	count=0;
+	while(count<(lenSend/1024))
+	{
+	memset(bufSend,0,sizeof(bufSend));
+	memset(outputBuf,0,sizeof(outputBuf));
+    fRet=read(fd,bufSend,1024);
+    if(fRet!=1024)
+    {
+        ScrClrLine(0,3);
+        ScrPrint(0,0,1,"file read err8");
+        return 1;
+    }
+	pack_up(bufSend,&fRet,outputBuf);
+	ScrClrLine(0,3);
+	ScrPrint(0,0,1,"SENDING DATA...");
+repeat:
+    comRet=PortSends(0,outputBuf,fRet);
+    if(comRet)
+    {
+        ScrClrLine(0,3);            //如果线被拔掉，重发
+        ScrPrint(0,0,1,"PortSends err comRet:%d",comRet);
+        goto repeat;
+
+    }
+    memset(bufRec,0,sizeof(bufRec));
+    outputLen=0;
+    ret=rcv_packet(bufRec,&outputLen);
+    switch(ret)
+    {
+        case 0:
+            break;
+        case 2:                   //if over time or plug off line ,send and rec again
+            ScrClrLine(0,3);
+            ScrPrint(0,0,1,"sending again");
+            goto repeat;
+    }
+	count++;
+	}//switch
+	fd=open("send",O_CREATE|O_RDWR);
+	if(fd<0)
+	{
+		ScrClrLine(0,3);
+		ScrPrint(0,0,1,"open file errt%d",errno);
+		return 1;
+	}
+	glFd=open("recv",O_CREATE|O_RDWR);
+	if(glFd<0)
+	{
+		ScrClrLine(0,3);
+		ScrPrint(0,0,1,"open recv err3%d",errno);
+		return 1;
+	}
+	for(j=0;j<10;j++) //read 10 times to compare from files
+	{
+		memset(getAllSend,0,sizeof(getAllSend));
+		memset(getAllRecv,0,sizeof(getAllRecv));
+		fRet=read(fd,getAllSend,10240);
+		if(fRet!=10240)
+		{
+			ScrClrLine(0,3);
+			ScrPrint(0,0,1,"file read err2y");
+			return 1;
+		}
+	
+		fRet=read(glFd,getAllRecv,10240);
+		if(fRet!=10240)
+		{
+			ScrClrLine(0,3);
+			ScrPrint(0,0,1,"file read err3");
+			return 1;
+		}
+		for(i=0;i<10240;i++) //compare 10kbs every time
+		{
+			if(getAllSend[i]!=getAllRecv[i])
+			{
+				ScrClrLine(0,3);
+				ScrPrint(0,0,1,"DATA DIFF:i=%d",i);
+				getkey();
+				return 1;
+			}
+		}
+	}
+	ScrClrLine(0,3);
+	ScrPrint(0,0,1,"SAME DATA");
+	PortClose(0);glCount=0;
+	close(fd);close(glFd);
+	//remove("send");remove("recv");
+	getkey();
 	return 0;
 }
 
@@ -204,161 +336,54 @@ int event_main(ST_EVENT_MSG *msg)
 
 int main(void)
 {
-    uchar bufSend[3000],bufRec[3000],outputBuf[3000],ucKey;
-	uchar getAllRecv[20000];uchar getAllSend[20000];
-	uchar randomBuf[9];
-    uint i,count,ret,comRet,lenSend;
-	ushort outputLen,fRet;
-	int fd,fdRet;
+    uchar ucKey,ret;
+	uint comRet;
+	int fd;
 
 	SystemInit();
     fd=open("send",O_CREATE|O_RDWR);
     if(fd<0)
     {
-        ScrClrLine(0,1);
+        ScrClrLine(0,3);
         ScrPrint(0,0,1,"open file err%d",errno);
         return 1;
     }
     glFd=open("recv",O_CREATE|O_RDWR);
     if(glFd<0)
     {
-        ScrClrLine(0,1);
+        ScrClrLine(0,3);
         ScrPrint(0,0,1,"open recv err%d",errno);
         return 1;
+    }
+	comRet=PortOpen(0,"115200,8,n,1");
+	if(comRet)
+    {
+	ScrClrLine(0,3);
+	ScrPrint(0,0,1,"PortOpen err0X%02X\n",comRet);
+	getkey();
+	return 1;
     }
 	while(1)
 	{
 		ScrCls();
 		kbflush();
-		ScrPrint(0,0,0x01|0x80,"1.TEST COMM     ");
-		ScrPrint(0,1,0x01|0x80,"2.CREATE FILE   ");
-
+		ScrPrint(0,0,0x01,"1.TEST COMM     ");
+		ScrPrint(0,2,0x01,"2.CREATE FILE   ");
 	do{
 	   ucKey=getkey();
 	} while(ucKey!=KEY1 && ucKey!=KEY2 && ucKey!=KEYENTER &&ucKey!=KEYCANCEL);
-	/*if(ucKey==KEY2)*/
 	switch(ucKey)
 	{
 	case KEY2:
-		for(i=0;i<1000;i++)
-		{
-        memset(randomBuf,0,sizeof(randomBuf));
-        PciGetRandom(randomBuf);
-        fRet=write(fd,randomBuf,8);
-        if(fRet!=8)
-        {
-            ScrClrLine(0,1);
-            ScrPrint(0,0,1,"file write err%d",i);
-            return 1;
-        }
-		}
+		ret=GeneData(fd);
+		if(1==ret)return 1;
 		break;
 	case KEY1:
 	case KEYENTER:
-		lenSend=8000;
-		fdRet=seek(fd,0,SEEK_SET);
-		if(fdRet<0)
-        {
-            ScrClrLine(0,1);
-            ScrPrint(0,0,1,"open file errr%d",errno);
-            return 1;
-        }
-		count=0;
-		while(count<(lenSend/400))
-		{
-		memset(bufSend,0,sizeof(bufSend));
-		memset(outputBuf,0,sizeof(outputBuf));
-        fRet=read(fd,bufSend,400);
-        if(fRet!=400)
-        {
-            ScrClrLine(0,1);
-            ScrPrint(0,0,1,"file read err8");
-            return 1;
-        }
-		pack_up(bufSend,&fRet,outputBuf);
-		ScrClrLine(0,1);
-		ScrPrint(0,0,1,"SENDING DATA...");
-repeat:
-		comRet=PortOpen(0,"115200,8,n,1");
-		if(comRet)
-        {
-		ScrClrLine(0,1);
-	    ScrPrint(0,0,1,"PortOpen err0X%02X\n",comRet);
-		getkey();
-		return 1;
-        }
-        comRet=PortSends(0,outputBuf,fRet);
-        if(comRet)
-        {
-            ScrClrLine(0,1);            //如果线被拔掉，重发
-            ScrPrint(0,0,1,"PortSends err comRet:%d",comRet);
-            goto repeat;
-
-        }
-        memset(bufRec,0,sizeof(bufRec));
-        outputLen=0;
-        ret=rcv_packet(bufRec,&outputLen);
-        switch(ret)
-        {
-         case 0:
-              break;
-         case 2:                   //if over time or plug off line ,send and rec again
-                PortClose(0);
-                ScrClrLine(0,1);
-                ScrPrint(0,0,1,"sending again");
-                goto repeat;
-        }
-		count++;
-		}//switch
-		fd=open("send",O_CREATE|O_RDWR);
-		if(fd<0)
-		{
-			ScrClrLine(0,1);
-			ScrPrint(0,0,1,"open file errt%d",errno);
-			return 1;
-		}
-		glFd=open("recv",O_CREATE|O_RDWR);
-		if(glFd<0)
-		{
-			ScrClrLine(0,1);
-			ScrPrint(0,0,1,"open recv err3%d",errno);
-			return 1;
-		}
-        memset(getAllSend,0,sizeof(getAllSend));
-        memset(getAllRecv,0,sizeof(getAllRecv));
-        fRet=read(fd,getAllSend,lenSend);
-        if(fRet!=lenSend)
-        {
-            ScrClrLine(0,1);
-            ScrPrint(0,0,1,"file read err2y");
-            return 1;
-        }
-        fRet=read(glFd,getAllRecv,lenSend);
-        if(fRet!=lenSend)
-        {
-            ScrClrLine(0,1);
-            ScrPrint(0,0,1,"file read err3");
-            return 1;
-        }
-        for(i=0;i<lenSend;i++)
-        {
-            if(getAllSend[i]!=getAllRecv[i])
-            {
-                ScrClrLine(0,1);
-                ScrPrint(0,0,1,"DATA DIFF:i=%d",i);
-                getkey();
-                return 1;
-            }
-        }
-		ScrClrLine(0,1);
-		ScrPrint(0,0,1,"SAME DATA");
-		PortClose(0);glCount=0;
-		close(fd);close(glFd);
-		//remove("send");remove("recv");
-		getkey();
+		ret=SendRecvData(fd);
+		if(1==ret)return 1;
 		break;//case KEY1 or KEYENTER
 		case KEYCANCEL:
-
 		      return 1;
 	}//switch
 	}//while(1)
