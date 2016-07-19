bbc3b7d7ce9aa2434127f8a38d4b4d113087c13e
xiehuan <2429753441@qq.com>
2016-07-19 19:02:24 +0800

    Update com_server.c
    增加拔插导致序列号在文件中没有存储好实现的开机与接收序列号进行比对，如果比接收的序列号小1，就加上1
    从而实现同步

--- comm_test/com_server.c
+++ comm_test/com_server.c
@@ -2,6 +2,8 @@
 
 #define COMPORT 0
 int glCount=0;
+int fd,fd1;
+int noSave;
 
 const APPINFO AppInfo={
 	"COM_SERVER",
@@ -104,7 +106,7 @@ void pack_up(const char *in_data,ushort *data_len,char *out_data)
 	if(in_data==NULL || *data_len==0 || out_data==NULL)
 	{
 		ScrClrLine(2,3);
-		ScrPrint(0,2,1,"PARA ERR");
+		ScrPrint(0,2,1,"PARA ERR %d",*data_len);
 		return;
 	}
 	dataLen=*data_len;
@@ -124,6 +126,7 @@ void pack_up(const char *in_data,ushort *data_len,char *out_data)
 	memcpy(dataPoint,crcBuf,2);
 	dataPoint+=2;
 	*data_len=(dataPoint-out_data);
+	glCount++;
 }
 
 int rcv_packet(char *packet,ushort *pack_len)
@@ -134,6 +137,7 @@ int rcv_packet(char *packet,ushort *pack_len)
 	uint offset,tempLen;
     ushort getCRC;
     uchar bufCRC[2];
+	int fid,temp;
 
 	memset(getData,0,sizeof(getData));
 	dataPoint=getData;
@@ -148,11 +152,27 @@ int rcv_packet(char *packet,ushort *pack_len)
 			getkey();
 			return 2;
 		}
+		if(kbhit()==0 && getkey()==KEYF1)
+		{
+			close(fd1);
+			if(0==remove("seq"))
+			{
+				ScrClrLine(0,3);
+				ScrPrint(0,0,1," seq cleared ,restart pls");
+				while(1);
+			}
+			else
+			{
+				ScrClrLine(0,3);
+				ScrPrint(0,0,1," seq cleared err%d",errno);
+				return 3;
+			}
+		}
 		comRet=PortRecv(COMPORT,&dataPoint[offset++],1000);
 		if(comRet)
 		{
 		ScrClrLine(0,1);
-		ScrPrint(0,0,1,"waitting...");
+		ScrPrint(0,0,1,"waitting...%d",glCount);
 		return 2;
 		}
         tempLen=(dataPoint[3]<<8)+dataPoint[4];
@@ -180,9 +200,67 @@ int rcv_packet(char *packet,ushort *pack_len)
 		ScrPrint(0,2,1,"CRC DIFF,%d",offset);
 		return 2;
 	}
-	glCount=(getData[1]<<8)+getData[2];
-	memcpy(packet,getData+5,offset-7);
-	*pack_len=offset-7;
+	noSave++;
+	if(noSave>=3) noSave=3;
+	switch(glCount-((getData[1]<<8)+getData[2]))
+	{
+	case 0:
+	case -1:
+		//glCount=(getData[1]<<8)+getData[2];
+		memcpy(packet,getData+5,offset-7);
+		*pack_len=offset-7;
+		if(noSave==1)   //保证上一次没有保存好数据，开机glCount+1;
+		{
+			glCount++;
+			noSave=2;
+		}
+		fid=seek(fd,0,SEEK_SET);
+		if(0!=fid)
+		{
+			ScrClrLine(2,3);
+			ScrPrint(0,2,1,"seek err%d",errno);
+			return 3;
+		}
+		fid=write(fd,(uchar*)packet,*pack_len);
+		if(fid!=*pack_len)
+		{
+			ScrClrLine(2,3);
+			ScrPrint(0,2,1,"write err");
+			return 3;
+		}
+	  break;
+	case 1:
+		/**
+		fileLen=filesize("data");
+		if(fileLen<0)
+		{
+			ScrClrLine(2,3);
+			ScrPrint(0,2,1,"filesize err");
+			return 3;
+		}
+		**/
+		fid=seek(fd,0,SEEK_SET);
+		if(0!=fid)
+		{
+		ScrClrLine(2,3);
+		ScrPrint(0,2,1,"seek err%d",errno);
+		return 3;
+		}
+		fid=read(fd,(uchar*)packet,1024);
+		if(fid<0)
+		{
+			ScrClrLine(2,3);
+			ScrPrint(0,2,1,"read err%d",1024);
+			return 3;
+		}
+		*pack_len=1024;
+		break;
+	default:
+		ScrClrLine(2,3);
+		ScrPrint(0,2,1,"sep err%d",glCount);
+		getkey();
+		return 2;
+	}//switch
 	return 0;
 }
 
@@ -191,6 +269,7 @@ uchar SendRecvData(void)
 	char bufRec[3000],outputBuf[3000];
     uint ret,comRet;
 	ushort outputLen;
+	int fid,temp;
 
 	while(1)
     {
@@ -211,6 +290,10 @@ uchar SendRecvData(void)
 	if(0x00==PortTxPoolCheck(COMPORT))
 	{
 	//DelayMs(500);
+	ScrClrLine(0,1);
+	ScrPrint(0,0,1,"sending%d,%d",glCount,(outputBuf[3]<<8)+outputBuf[4]);
+	if(glCount==100) glCount=0;
+	
     comRet=PortSends(COMPORT,(uchar*)outputBuf,outputLen);
     if(comRet)
     {
@@ -218,8 +301,21 @@ uchar SendRecvData(void)
         ScrPrint(0,2,1,"PortSends Ret:%d",comRet);
         return 2;
     }
-	ScrClrLine(0,1);
-	ScrPrint(0,0,1,"sending%d,%d",glCount,(outputBuf[3]<<8)+outputBuf[4]);
+	fid=seek(fd1,0,SEEK_SET);
+	if(0!=fid)
+	{
+		ScrClrLine(2,3);
+		ScrPrint(0,2,1,"seek fd1 err%d",errno);
+		return 3;
+	}
+	temp=glCount-1;
+	fid=write(fd1,&temp,1);
+	if(fid!=1)
+	{
+		ScrClrLine(2,3);
+		ScrPrint(0,2,1,"write fd1 err");
+		return 3;
+	}
 	}
     }
 	PortClose(COMPORT);
@@ -235,8 +331,25 @@ int main(void)
 {
     uchar ret;
 	uint comRet;
+	int fid;
 
 	SystemInit();
+	fd=open("data",O_CREATE | O_RDWR);
+	if(fd<0)
+	{
+		ScrClrLine(2,3);
+		ScrPrint(0,2,1,"open file err%d",errno);
+		return 1;
+	}
+	fd1=open("seq",O_CREATE | O_RDWR);
+	if(fd1<0)
+	{
+		ScrClrLine(2,3);
+		ScrPrint(0,2,1,"open file seq err%d",errno);
+		return 1;
+	}
+	fid=read(fd1,&glCount,1);
+	if(1!=fid ) glCount =0;
 	comRet=PortOpen(COMPORT,"115200,8,n,1");
 	if(comRet)
     {
