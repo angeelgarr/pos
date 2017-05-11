2de75c7c01e3ad3375eb804ce1a2ed4504045310
xiehuan <2429753441@qq.com>
2017-05-11 10:36:18 -0500

    Update readCurl.c
    every time read once.

--- readCurl.c
+++ readCurl.c
@@ -22,12 +22,13 @@ int main(void)
 	fseek(stream,0,SEEK_SET);
     while(1)
     {
-    	iLen = fread(&buff[j++],1,1,stream);
+    	iLen = fread(&buff[j],100,1,stream);
     	printf("iLen=%d",iLen);
     	if(iLen <= 0)
     	{
     		break;
     	}
+	j +=`100;
     }
 	fclose(stream);
 	printf("\nbuff=%s\n",buff);
