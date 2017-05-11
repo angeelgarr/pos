09e59e5656281e24de17f82376383c25affbd4d8
x-lugoo <2429753441@qq.com>
2017-05-11 22:07:29 +0800

    add readCurlFIle

---
+++ readCurl.c
@@ -0,0 +1,37 @@
+/*
+ * test.c
+ *
+ *  Created on: 2017年5月1日
+ *      Author: jeff
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int iFd;
+	FILE *stream;
+	char buff[100] = {0};
+	stream = fopen("tempko","w+");
+	int j = 0;
+	int iLen;
+
+	for(int i = 0;i < 3;i++){
+		fwrite("buff",4,1,stream);
+	}
+	fseek(stream,0,SEEK_SET);
+    while(1)
+    {
+    	iLen = fread(&buff[j++],1,1,stream);
+    	printf("iLen=%d",iLen);
+    	if(iLen <= 0)
+    	{
+    		break;
+    	}
+    }
+	fclose(stream);
+	printf("\nbuff=%s\n",buff);
+
+	return 0;
+}
+
