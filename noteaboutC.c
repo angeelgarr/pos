8128408a5ca6e0fea0581461dfb42e0f43e197c5
xiehuan <2429753441@qq.com>
2017-07-24 12:39:38 +0800

    Create noteaboutC.c

---
+++ noteaboutC.c
@@ -0,0 +1,17 @@
+int first(void)
+{
+  return 0;
+ }
+ 
+ int main(void)
+ {
+   if(first()){
+    printf("first return error\n");
+   }else{
+   printf("first return right\n");
+   }
+   
+   return 0;
+  }
+ 
+ 
